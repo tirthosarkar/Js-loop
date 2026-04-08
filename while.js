24a3d4cc3a0e904a92b412f24d0cf6d18ee479b3
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-09 01:01:36 +0600

    added while.js

---
+++ while.js
@@ -0,0 +1,13 @@
+const number = 7;
+if (number < 10) {
+  console.log('choto ekta number paisi');
+}
+console.log('something new is cooling');
+
+//----------WHILE--LOOP--------------//
+let num = 0;
+while (num < 5) {
+  console.log('looooooing', num);
+  // num = num + 1;
+  // num += 1;
+}
