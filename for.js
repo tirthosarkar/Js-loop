dab9fb43e85dcac7d4168245a6899bac55830e01
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-10 11:08:35 +0600

    added more code for

---
+++ for.js
@@ -0,0 +1,21 @@
+/**
+ * for (     ;   ; ){
+ * }
+ *
+ *for (first parent; second parent; third part) {
+ *loop statement
+ *}
+ *
+ * 
+*/
+
+// let num = 0; //loop variable
+// while (num < 5) {
+//   //loop statement
+//   console.log(num); //loop variable change
+//   num++;
+// }
+
+for (let num = 0; num < 5; num++){
+  console.log(num);
+}
