b34db63ac68fcab82b1a6802f39220927d175c30
tirthosarkar <tirthosarkar1@gmail.com>
2026-04-10 21:56:23 +0600

    added more code break

---
+++ breack.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 15; i ++){
+  console.log(i);
+  if (i >= 5) {
+    break
+  }
+}
+
+console.log('life after break up')
+
+let n = 54;
+while (n > 25){
+  console.log(n);
+    if(n <50){
+    break;
+  }
+  n--;
+}
